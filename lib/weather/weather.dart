ece9d596da3ba999eec59b252a082bd5ab61d800
eduardoagas <eagasilva12@gmail.com>
2023-07-15 00:54:16 -0300

    all tests passed!

---
+++ lib/weather/weather.dart
@@ -0,0 +1,7 @@
+export 'models/models.dart';
+export 'cubit/weather_cubit.dart';
+export '../weather/view/weather_page.dart';
+export '../weather/view/widgets/weather_empty.dart';
+export '../weather/view/widgets/weather_error.dart';
+export '../weather/view/widgets/weather_loading.dart';
+export '../weather/view/widgets/weather_populated.dart';
